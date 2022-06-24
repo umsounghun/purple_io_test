e182f43018cb7ab73adca0f1629ff04220d3f2dd
Sounghun Um <korea970112@naver.com>
2022-06-24 21:27:10 +0900

    fix : 필수 테스트 완료

--- src/main/java/com/sounghun/purple_io_test/RequiredTest/Second_searchNumber.java
+++ src/main/java/com/sounghun/purple_io_test/RequiredTest/Second_searchNumber.java
@@ -1,35 +1,40 @@
 package com.sounghun.purple_io_test.RequiredTest;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Second_searchNumber {
 
-    final int month = 3;
-
-    // 1.분기_숫자_구하기
-    public static int month(int month) {
-        int answer = 0;
-        // 1 ~ 12월이 아닌 경우
-        if (month < 1 || month > 12) {
-            throw new IllegalArgumentException("올바른 값이 아닙니다");
-        }
-
-        if (month % 3 == 0) {
-            answer = (month / 3);
-        } else {
-            answer = (month / 3) + 1;
-        }
-        return answer;
-    }
+    static final int [] ids = {0,1,2,3,4,5,6};
 
     /**
-     * 2.사용하지 않는 가장 작은 숫자 찾기
-     * 조건 1. 정렬되지 않는 배열을 넣어도 동작해야 함
-     * 조건 2. 중복된 숫자가 있는 경우도 동작해야 함
-     */
+     * 2.사용하지_않는_가장_작은_숫자_찾기
+     * 정렬되지 않는 배열을 넣어도 동작해야함
+     * 중복된 숫자가 있는 경우도 동작해야함
+      */
+    public static void searchMissNum (int[] ids)
+    {
+        int left = 0, right = ids.length - 1;
+        System.out.println("The smallest missing element is "
+                + findSmallestMissing(ids, left, right));
+    }
 
+    public static int findSmallestMissing(int[] ids, int left, int right)
+    {
+        // 기본 조건
+        if (left > right) {
+            return left;
+        }
 
+        int mid = left + (right - left) / 2;
+
+        // 중간 인덱스가 값과 일치하면 불일치
+        // 오른쪽 절반에 위치
+        if (ids[mid] == mid) {
+            return findSmallestMissing(ids, mid + 1, right);
+        }
+        else {
+            // 불일치는 왼쪽 절반에 있습니다.
+            return findSmallestMissing(ids, left, mid - 1);
+        }
+    }
 
 
 }
